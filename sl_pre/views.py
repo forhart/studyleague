bc9c248ef817c7f8c676271df440506c70615abc
Foras <foraart@gmail.com>
2012-03-29 19:32:13 +0530

    Subject list working... Set up templates dir write url, view
    in View Execute query and pass a context to template.

--- sl_pre/views.py
+++ sl_pre/views.py
@@ -1,5 +1,8 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from sl_pre.models import Stream
 
 def streams_list(request):
-    return HttpResponse("Hello WOrld")
+    streams = Stream.objects.all()
+    return render_to_response('streams.html',{'streams_list': streams })
